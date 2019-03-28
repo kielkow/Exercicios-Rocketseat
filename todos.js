4f438b210c6f5a72b24ae7ece905877696d0cec2
Matheus Kielkowski <matheuskiel@fiorifer.com.br>
2019-03-27 23:53:57 -0300

    Add files via upload

--- todos.js
+++ todos.js
@@ -2,11 +2,7 @@ var listElement = document.querySelector('#app ul')
 var inputElement = document.querySelector('#app input')
 var buttonElement = document.querySelector('#app button')
 
-var todos = [
-    'Fazer café',
-    'Estudar Javascript',
-    'Acessar comunidade Rocketseat'
-]
+var todos = JSON.parse(localStorage.getItem('list_todos')) || []
 
 function renderTodos(){
 
@@ -42,6 +38,7 @@ function addTodo(){
     todos.push(todoText)
     inputElement.value = ''
     renderTodos()
+    saveToStorage()
 }
 
 buttonElement.onclick = addTodo
@@ -49,4 +46,9 @@ buttonElement.onclick = addTodo
 function deleteTodo(pos){
     todos.splice(pos, 1)  //(posicao do vetor, quantidade itens a serem removidos)
     renderTodos()
+    saveToStorage()
+}
+
+function saveToStorage(){
+    localStorage.setItem('list_todos', JSON.stringify(todos))
 }
