bde2df32517ac0cadb4d15d51634c116291f400a
Matheus Kielkowski <matheuskiel@fiorifer.com.br>
2019-03-28 22:56:14 -0300

    Add files via upload

--- modulo3_4.js
+++ modulo3_4.js
@@ -22,12 +22,40 @@ checaIdade(15)
 */
 
 //Exercicio 2 e 3
-axios.get('https://api.github.com/users/kielkow')
+var inputElement = document.querySelector('#app input')
+var buttonElement = document.querySelector('#app button')
+var listElement = document.querySelector('#app ul')
+//var repos = JSON.parse(localStorage.getItem('list_repos')) || []
 
-    .then(function (response) {
-        console.log(response.data.login)
-    })  //metodo chamado pelo resolve
 
-    .catch(function (error) {
-        console.warn(error)
-    })    //metodo chamado pelo reject 
+function buscar() {
+
+    listElement.innerHTML = ''
+
+    var loadElement = document.createElement('li')
+    var loadText = document.createTextNode('Loading ...')
+    loadElement.appendChild(loadText)
+    listElement.appendChild(loadElement)
+
+    userText = inputElement.value
+    axios.get('https://api.github.com/users/' + userText + '/repos')
+        .then(function (response) {
+            var x;
+            for (x in response.data) {
+                console.log(response.data[x].name)
+                console.log(x)
+                var repoElement = document.createElement('li')
+                var repoText = document.createTextNode(response.data[x].name)
+                repoElement.appendChild(repoText)
+                listElement.appendChild(repoElement)
+            }
+            listElement.removeChild(loadElement)
+        })
+
+        .catch(function (error) {
+            alert('Error 404 - Not found user')
+            listElement.removeChild(loadElement)
+        })
+}
+
+buttonElement.onclick = buscar
